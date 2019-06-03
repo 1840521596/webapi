5f40343744dc11158e81033c24633b8bf5bb9c60
guohongjie <guohongjie@yunshuxie.com>
2019-06-03 11:38:31 +0800

    guohongjie add

--- app/base/pythonProject/base/template.py
+++ app/base/pythonProject/base/template.py
@@ -40,16 +40,18 @@ getSetUpText = """        params = {{caseRequestDatas}}
         self.url = "{{apiHost}}"+"{{apiUrl}}"
         caseHeaders = {{caseHeaders}}
         self.resp = requests.get(self.url, params=params, headers=caseHeaders ,cookies=self.cookies)
+        print self.resp.content
         logging.info(self.url + lianjiefu + self.resp.text +fengefu )
         self.cookies.update(self.resp.cookies)"""
 postSetUpText = """        data = {{caseRequestDatas}}
         self.url = "{{apiHost}}"+"{{apiUrl}}"
         caseHeaders = {{caseHeaders}}
         self.resp = requests.post(self.url, data=data, headers=caseHeaders ,cookies=self.cookies)
+        print self.resp.content
         logging.info(self.url + lianjiefu + self.resp.text +fengefu )
         self.cookies.update(self.resp.cookies)"""
 postFunctionText="""    def test_{{apiNameEN}}(self):
-        \"\"\"{{caseDesc}}\"\"\"
+        \"\"\"{{caseDesc}}<br/>{apiUrl}<br/>{{caseRequestDatas}}<br/>\"\"\"
         self.caseStatusCode = {{caseStatusCode}}
         self.caseExpectDatas ={{caseExpectDatas}}
         data = {{caseRequestDatas}}
@@ -57,12 +59,13 @@ postFunctionText="""    def test_{{apiNameEN}}(self):
         method = "{{method}}"
         caseHeaders = {{caseHeaders}}
         self.resp = requests.post(self.url, data=data, headers=caseHeaders,cookies=self.cookies)
+        print self.resp.content
         logging.info(self.url + lianjiefu + self.resp.text +fengefu )
         msg = \"\"\"\n        Except:  {Except}-*-\n        Really:  {Really}\"\"\"  #校验HTTP返回代码
         assert self.caseStatusCode==self.resp.status_code,msg.format(Except=self.caseStatusCode,Really=self.resp.status_code)
         {{assertValue}}"""
 getFunctionText="""    def test_{{order}}_{{apiNameEN}}(self):
-        \"\"\"{{caseDesc}}\"\"\"
+        \"\"\"{{caseDesc}}<br/>{apiUrl}<br/>{{caseRequestDatas}}<br/>\"\"\"
         self.caseStatusCode = {{caseStatusCode}}
         self.caseExpectDatas ={{caseExpectDatas}}
         params = {{caseRequestDatas}}
@@ -70,6 +73,7 @@ getFunctionText="""    def test_{{order}}_{{apiNameEN}}(self):
         method = "{{method}}"
         caseHeaders = {{caseHeaders}}
         self.resp = requests.get(self.url, params=params, headers=caseHeaders,cookies=self.cookies)
+        print self.resp.content
         logging.info(self.url + lianjiefu + self.resp.text +fengefu )
         msg = \"\"\"\n        Except:  {Except}-*-\n        Really:  {Really}\"\"\"  #校验HTTP返回代码
         assert self.caseStatusCode==self.resp.status_code,msg.format(Except=self.caseStatusCode,Really=self.resp.status_code)
