9b20433e07f55679a544dc04db152cfb93875126
guohongjie <guohongjie@yunshuxie.com>
2019-06-11 11:04:26 +0800

    guohongjie update

--- app/base/pythonProject/base/getCrmCookies.py
+++ app/base/pythonProject/base/getCrmCookies.py
@@ -55,7 +55,59 @@ def get_crm_cookie(env_flag,env_num):
         return cookies
     else:
         return(get_crm_cookie(env_flag,env_num))  # 递归
+
+def get_admin_cookie(env_flag,env_num):
+    """ 登录crm, 并返回cookies
+    :param url 请求连接
+    :param header 请求头
+    :return cookies
+    """
+    url = r"https://admin.yunshuxie.com/common_index/loginIn.json"
+    cookies = requests.cookies.RequestsCookieJar()  # 生成cookies 容器
+    cookies.set('env_flag', env_flag)  # 设置测试环境
+    cookies.set("env_num", env_num)  # 设置环境号
+    header = {"Accept": "application/json, text/javascript, */*; q=0.01",
+              "Accept-Encoding": "gzip, deflate, br",
+              "Accept-Language": "zh-CN,zh;q=0.9",
+              "Cache-Control": "no-cache","Connection": "keep-alive",
+              "Content-Length": "73",
+              "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+              "Origin": "https://admin.yunshuxie.com",
+              "Pragma": "no-cache","Referer": "https://admin.yunshuxie.com/","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36","X-Requested-With": "XMLHttpRequest"}
+    params = {"userName": "automation@yunshuxie.com" ,"pwd": "ysx2019"}
+    resp = requests.post(url=url, headers=header, cookies=cookies,data=params)
+    dict_resp =json.loads(resp.content, encoding="utf8")
+    print dict_resp
+    if dict_resp["returnCode"] == "0" or dict_resp["returnCode"] == 0:
+        cookies.update(resp.cookies)
+    else:
+        raise Exception, resp.content
+    return cookies.get_dict()
+def get_web_home_cookie(env_flag,env_num):
+    """ 登录PC云舒写官网, 并返回cookies
+    :param url 请求连接
+    :param header 请求头
+    :return cookies
+    """
+    url = r"https://www.yunshuxie.com/v5/web/account/login.htm"
+    cookies = requests.cookies.RequestsCookieJar()  # 生成cookies 容器
+    cookies.set('env_flag', env_flag)  # 设置测试环境
+    cookies.set("env_num", env_num)  # 设置环境号
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+              "Accept": "application/json, text/javascript, */*; q=0.01",
+              "Accept-Encoding": "gzip, deflate, br","Accept-Language": "zh-CN,zh;q=0.9",
+              "Connection": "keep-alive","Host": "www.yunshuxie.com","Upgrade-Insecure-Requests": "1"}
+    params = {"userName": "60000008100" ,"pwd": "123456"}
+    resp = requests.post(url=url, headers=header, cookies=cookies,data=params)
+    dict_resp = json.loads(resp.content, encoding="utf8")
+    print dict_resp
+    if dict_resp["returnCode"] == "0" or dict_resp["returnCode"] == 0:
+        cookies.update(resp.cookies)
+    else:
+        raise Exception, resp.content
+    return cookies.get_dict()
+
 if __name__ == "__main__":
-    print get_crm_cookie("stage","1")
+    print get_web_home_cookie("beta","1")
 
 
