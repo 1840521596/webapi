69ea5eb11b9cf77069d72b8b7ee5665bc0b7d8f3
guohongjie <guohongjie@yunshuxie.com>
2019-06-14 19:47:02 +0800

    guohongjie

--- app/main/httpSuite.py
+++ app/main/httpSuite.py
@@ -127,7 +127,7 @@ def runDatasApiTest_yunwei():
 			redis_host = s.get_env("beta").split(":") if env_flag == "beta" else s.get_env("prod_stage").split(":")
 			r = red.Redis(host=redis_host[0], port=int(redis_host[1]), password="yunshuxie1029Password")
 			r.set("021ZaJtG17hM310SblvG1NZutG1ZaJtQ",'o38sIv_7FQInsBKJEUExn7wYxoHc&21_bk4dQIEFnYz5w8zJwDqan84UFmV_XVKEO5MJf7fv1pGR8tRH2MAtxpk0Pc1SqDwe5S90CE6TQo1wd346qEA5FQ')
-			if "admin".upper() not in project_en[0].upper() and "crm".upper() not in project_en[0].upper():  # 判断项目不等于admin&&crm，新增测试用户
+			if "admin".upper() not in project_en[0].upper() and "crm".upper() not in project_en[0].upper() and "wacc_tortoise".upper() not in project_en[0].upper():  # 判断项目不等于admin&&crm，新增测试用户
 				try:
 					if env_flag in ["stage", "prod"]:
 						new_env_flag = ",".join(["stage", "prod"])
