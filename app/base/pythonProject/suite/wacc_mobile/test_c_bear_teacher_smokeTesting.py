f41f5cfe3af1399c81524a6915b36ee1df818659
guohongjie <guohongjie@yunshuxie.com>
2019-08-02 20:16:49 +0800

    guohongjie

--- app/base/pythonProject/suite/wacc_mobile/test_c_bear_teacher_smokeTesting.py
+++ app/base/pythonProject/suite/wacc_mobile/test_c_bear_teacher_smokeTesting.py
@@ -314,7 +314,7 @@ class BearWord_Teacher_Test(unittest.TestCase):
         header = {"Connection": "keep-alive",  # "Content-Type": "multipart/form-data",
                   "User-Agent": "BearWord/1.0.0 (iPhone; iOS 12.3.1; Scale/3.00)"}
         files = {
-            'file': ("mp3.amr",open(r'mp3.amr', 'rb'),"multipart/form-data"),
+            'file': ("mp3.amr",open(r'/home/work/ysx-webapi/app/base/pythonProject/suite/wacc_mobile/mp3.amr', 'rb'),"multipart/form-data"),
         }
         str_params = """{'file': ("mp3.amr",open(r'mp3.amr', 'rb'),"multipart/form-data")}"""
         # logging.info(url + lianjiefu + str_params + fengefu)
