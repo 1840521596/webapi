b2d9fe89da0710b223268bc30ab15b57528e3ae4
guohongjie <guohongjie@yunshuxie.com>
2019-08-06 13:55:19 +0800

    guohongjie

--- app/base/pythonProject/suite/wacc_mobile/test_c_bear_teacher_smokeTesting.py
+++ app/base/pythonProject/suite/wacc_mobile/test_c_bear_teacher_smokeTesting.py
@@ -345,7 +345,7 @@ class BearWord_Teacher_Test(unittest.TestCase):
             header = {"Connection": "keep-alive", "Content-Type": "application/x-www-form-urlencoded",
                       "User-Agent": "BearWord/1.0.0 (iPhone; iOS 12.3.1; Scale/3.00)"}
             cookies = get_app_cookie(self.env_flag, self.env_num, self.phone)
-            params = {"timeLineId":bearWord_timelineId,"excellence":"0","commentVoice":commentVoice,"commentContent":"测试作业批改","submitUpdateDate":bearWord_submitUpdateDate}
+            params = {"timeLineId":bearWord_timelineId,"excellence":"0","commentVoice":commentVoice,"commentContent":"[]","submitUpdateDate":bearWord_submitUpdateDate}
             # logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
             str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
             print str_params
@@ -365,6 +365,7 @@ class BearWord_Teacher_Test(unittest.TestCase):
             raise Exception, u"当前教师未存在待批改作业"
     def test_15_v1_bear_teacher_corrected_record_list(self):
         """老师端：批改记录<br>https://mobile.yunshuxie.com/v1/bear/teacher/corrected_record_list.htm<br>{"page":"1","pageSize":"10","jobType":"0"}"""
+        time.sleep(5)
         url = r"https://mobile.yunshuxie.com"+r"/v1/bear/teacher/corrected_record_list.htm"
         params = {"page":"1","pageSize":"10","jobType":"0"}
         # logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
