bc48813e05cd762e9fac1761ed43662fba069d46
guohongjie <guohongjie@yunshuxie.com>
2019-08-06 11:44:42 +0800

    guohongjie

--- app/base/pythonProject/run.py
+++ app/base/pythonProject/run.py
@@ -18,7 +18,8 @@ def get_allcase(project):
     case_path = os.path.join(TEST_FOLDER +"/suite/",project)
     dis = unittest.TestLoader()
     discover = dis.discover(case_path, pattern="test*.py")
-    print discover
+    print type(discover)
+    print discover._tests
     suite = unittest.TestSuite()
     suite.addTest(discover)
     return suite
