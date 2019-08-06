4b3b773e40c42edd457884f449022c2fecc547cd
guohongjie <guohongjie@yunshuxie.com>
2019-08-06 14:10:02 +0800

    guohongjie

--- app/base/pythonProject/suite/wacc_mobile/test_d_bear_Student_and_Teacher_smokeTesting.py
+++ app/base/pythonProject/suite/wacc_mobile/test_d_bear_Student_and_Teacher_smokeTesting.py
@@ -158,6 +158,9 @@ class BearWord_Student_and_Teacher_Test(unittest.TestCase):
             url = r"http://mobile.yunshuxie.com" + r"/v1/bear/student/commentTeacher.htm"
             params = {"workId": workId, "teacherId": teacherId, "commentContent": "测试评价教师功能", "commentStar": "5",
                       "isApp": "2"}
+            str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
+            print str_params
+            #logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
             cookies = get_app_cookie(self.env_flag, self.env_num, phone=self.phone)
             self.resp = requests.post(url=url, headers=self.header, cookies=cookies, data=params)
             print self.resp.text
@@ -173,83 +176,83 @@ class BearWord_Student_and_Teacher_Test(unittest.TestCase):
         else:
             print u"未存在教师已点评的课程作品数据"
             raise Exception, u"未存在教师已点评的课程作品数据"
-    # def test_09_v1_bear_teacher_recommend_job(self):
-    #     """老师端：推荐/取消推荐优秀作业-取消推荐<br>https://mobile.yunshuxie.com/v1/bear/teacher/recommend_job.htm.htm<br>{"timeLineId":"","excellence":"0"}"""
-    #     bearWord_timelineId = self.redis.str_get("bearWord_timelineId") if self.redis.str_get("bearWord_timelineId") else None
-    #     if bearWord_timelineId:
-    #         url = r"https://mobile.yunshuxie.com"+r"/v1/bear/teacher/recommend_job.htm"
-    #         params = {"timeLineId":bearWord_timelineId,"excellence":"0"}
-    #         # logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
-    #         str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
-    #         print str_params
-    #         cookies = get_app_cookie(self.env_flag, self.env_num, self.phone)
-    #         self.session.cookies = cookies
-    #         self.resp = self.session.post(url=url,data=params)
-    #         print self.resp.text
-    #         result = json.loads(self.resp.text, encoding="utf8")
-    #         #logging.info(url + lianjiefu + self.resp.content + fengefu)
-    #         expect = {"returnCode": "0"}
-    #         if result["returnCode"] == "0" or result["returnCode"] == 0:
-    #             assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
-    #                                                                                  Really=result["returnCode"])
-    #         else:
-    #             assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
-    #                                                                                  Really=result["returnCode"])
-    #     else:
-    #         print u"当前教师未存在待批改作业"
-    #         raise Exception,u"当前教师未存在待批改作业"
-    # def test_10_bear_teacher_reject_job(self):
-    #     """作业退回接口-教师端<br>https://mobile.yunshuxie.com/v1/bear/teacher/return_job.htm<br/>{"timeLineId":""}"""
-    #     if self.env_flag != "beta":
-    #         print u"当前运行环境非beta,跳过打卡接口"
-    #     else:
-    #         workId = self.redis.str_get("bearWord_workId") if self.redis.str_get("bearWord_workId") else None
-    #         if workId:
-    #             url = r"http://mobile.yunshuxie.com" + "/v1/bear/teacher/return_job.htm"
-    #             params = {"timeLineId":workId}
-    #             #logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
-    #             str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
-    #             print str_params
-    #             self.resp = self.session.post(url=url, data=params)
-    #             print self.resp.text
-    #             result = json.loads(self.resp.text, encoding="utf8")
-    #             #logging.info(url + lianjiefu + self.resp.text + fengefu)
-    #             expect = {"returnCode": "0"}
-    #             if result["returnCode"] == "0" or result["returnCode"] == 0:
-    #                 assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
-    #                                                                                      Really=result["returnCode"])
-    #             else:
-    #                 assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
-    #                                                                                      Really=result["returnCode"])
-    #         else:
-    #             print u"当前页面不存在章节数据"
-    #             raise Exception, u"当前页面不存在章节数据"
-    # def test_11_bear_teacher_reject_job(self):
-    #     """ 驳回接口-教师端<br>https://mobile.yunshuxie.com/v1/bear/teacher/reject_job.htm<br/>{"timeLineId":"","reasonId":"","status":""}"""
-    #     if self.env_flag != "beta":
-    #         print u"当前运行环境非beta,跳过打卡接口"
-    #     else:
-    #         workId = self.redis.str_get("bearWord_workId") if self.redis.str_get("bearWord_workId") else None
-    #         if workId:
-    #             url = r"http://mobile.yunshuxie.com" + "/v1/bear/teacher/reject_job.htm"
-    #             params = {"timeLineId":workId,"reasonId":"1","status":"1"}
-    #             #logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
-    #             str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
-    #             print str_params
-    #             self.resp = self.session.post(url=url, data=params)
-    #             print self.resp.text
-    #             result = json.loads(self.resp.text, encoding="utf8")
-    #             #logging.info(url + lianjiefu + self.resp.text + fengefu)
-    #             expect = {"returnCode": "0"}
-    #             if result["returnCode"] == "0" or result["returnCode"] == 0:
-    #                 assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
-    #                                                                                      Really=result["returnCode"])
-    #             else:
-    #                 assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
-    #                                                                                      Really=result["returnCode"])
-    #         else:
-    #             print u"当前页面不存在章节数据"
-    #             raise Exception, u"当前页面不存在章节数据"
+    def test_07_v1_bear_teacher_recommend_job(self):
+        """老师端：推荐/取消推荐优秀作业-取消推荐<br>https://mobile.yunshuxie.com/v1/bear/teacher/recommend_job.htm.htm<br>{"timeLineId":"","excellence":"0"}"""
+        bearWord_timelineId = self.redis.str_get("bearWord_timelineId") if self.redis.str_get("bearWord_timelineId") else None
+        if bearWord_timelineId:
+            url = r"https://mobile.yunshuxie.com"+r"/v1/bear/teacher/recommend_job.htm"
+            params = {"timeLineId":bearWord_timelineId,"excellence":"0"}
+            # logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
+            str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
+            print str_params
+            cookies = get_app_cookie(self.env_flag, self.env_num, self.phone)
+            self.session.cookies = cookies
+            self.resp = self.session.post(url=url,data=params)
+            print self.resp.text
+            result = json.loads(self.resp.text, encoding="utf8")
+            #logging.info(url + lianjiefu + self.resp.content + fengefu)
+            expect = {"returnCode": "0"}
+            if result["returnCode"] == "0" or result["returnCode"] == 0:
+                assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
+                                                                                     Really=result["returnCode"])
+            else:
+                assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
+                                                                                     Really=result["returnCode"])
+        else:
+            print u"当前教师未存在待批改作业"
+            raise Exception,u"当前教师未存在待批改作业"
+    def test_08_bear_teacher_reject_job(self):
+        """作业退回接口-教师端<br>https://mobile.yunshuxie.com/v1/bear/teacher/return_job.htm<br/>{"timeLineId":""}"""
+        if self.env_flag != "beta":
+            print u"当前运行环境非beta,跳过打卡接口"
+        else:
+            workId = self.redis.str_get("bearWord_workId") if self.redis.str_get("bearWord_workId") else None
+            if workId:
+                url = r"http://mobile.yunshuxie.com" + "/v1/bear/teacher/return_job.htm"
+                params = {"timeLineId":workId}
+                #logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
+                str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
+                print str_params
+                self.resp = self.session.post(url=url, data=params)
+                print self.resp.text
+                result = json.loads(self.resp.text, encoding="utf8")
+                #logging.info(url + lianjiefu + self.resp.text + fengefu)
+                expect = {"returnCode": "0"}
+                if result["returnCode"] == "0" or result["returnCode"] == 0:
+                    assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
+                                                                                         Really=result["returnCode"])
+                else:
+                    assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
+                                                                                         Really=result["returnCode"])
+            else:
+                print u"当前页面不存在章节数据"
+                raise Exception, u"当前页面不存在章节数据"
+    def test_09_bear_teacher_reject_job(self):
+        """ 驳回接口-教师端<br>https://mobile.yunshuxie.com/v1/bear/teacher/reject_job.htm<br/>{"timeLineId":"","reasonId":"","status":""}"""
+        if self.env_flag != "beta":
+            print u"当前运行环境非beta,跳过打卡接口"
+        else:
+            workId = self.redis.str_get("bearWord_workId") if self.redis.str_get("bearWord_workId") else None
+            if workId:
+                url = r"http://mobile.yunshuxie.com" + "/v1/bear/teacher/reject_job.htm"
+                params = {"timeLineId":workId,"reasonId":"1","status":"1"}
+                #logging.info(url + lianjiefu + json.dumps(params, ensure_ascii=False) + fengefu)
+                str_params = json.dumps(params, ensure_ascii=False, encoding="utf8")
+                print str_params
+                self.resp = self.session.post(url=url, data=params)
+                print self.resp.text
+                result = json.loads(self.resp.text, encoding="utf8")
+                #logging.info(url + lianjiefu + self.resp.text + fengefu)
+                expect = {"returnCode": "0"}
+                if result["returnCode"] == "0" or result["returnCode"] == 0:
+                    assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
+                                                                                         Really=result["returnCode"])
+                else:
+                    assert result["returnCode"] == expect["returnCode"], self.msg.format(Expect=expect["returnCode"],
+                                                                                         Really=result["returnCode"])
+            else:
+                print u"当前页面不存在章节数据"
+                raise Exception, u"当前页面不存在章节数据"
 
     @classmethod
     def tearDownClass(self):
