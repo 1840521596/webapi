a4a2c7b2ef63108205828ea43923c57f372f3866
guohongjie <guohongjie@yunshuxie.com>
2019-10-21 16:12:39 +0800

    guohongjie

--- app/config/project_loginIn.py
+++ app/config/project_loginIn.py
@@ -15,7 +15,7 @@ def loginIn(project_cn,env_flag,env_num,account):
         new_cookies = get_wacc_tortoise_cookie(env_flag,env_num, account)
     elif project_cn == "新商品详情系统" or project_cn == "新订单支付系统":
         new_cookies = get_wacc_bird_cookie(env_flag,env_num, account)
-    elif project_cn == ["罐罐熊APP","云舒写APP"]:
+    elif project_cn in ["罐罐熊APP","云舒写APP"]:
         new_cookies = get_app_cookie(project_cn,env_flag,env_num, account)
     elif project_cn == "陪你阅读陪你写作":
         new_cookies = get_wechat_cookie(env_flag,env_num, account)
